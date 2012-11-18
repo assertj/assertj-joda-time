52e4460a391c3c8c8005dd906df98cdc4ed24b60
Joel Costigliola <joel.costigliola@gmail.com>
2012-11-18 18:52:05 +0100

    Initialize project structure and pom

---
+++ src/main/java/org/fest/assertions/api/JODA_TIME.java
@@ -0,0 +1,11 @@
+package org.fest.assertions.api;
+
+
+/**
+ * The entry point for all Joda Time assertions.
+ * 
+ * @author Joel Costigliola
+ */
+public class JODA_TIME  {
+  // TODO fill with assertions
+}

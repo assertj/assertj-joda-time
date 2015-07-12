96a067d54c87a808e6554faab2cbb91c90235899
Joel Costigliola <joel.costigliola@gmail.com>
2015-07-12 22:51:45 +1200

    update classes javadoc licence fix unit tests by making expectException method use String.format

--- src/test/java/org/assertj/jodatime/api/JodaTimeBaseTest.java
+++ src/test/java/org/assertj/jodatime/api/JodaTimeBaseTest.java
@@ -8,7 +8,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  *
- * Copyright 2012-2013 the original author or authors.
+ * Copyright 2012-2015 the original author or authors.
  */
 package org.assertj.jodatime.api;
 
@@ -28,7 +28,7 @@ public class JodaTimeBaseTest {
     thrown.expect(type);
     thrown.expectMessage(format(message));
   }
-  
+
   protected void expectIllegalArgumentException(String message) {
     expectException(IllegalArgumentException.class, format(message));
   }
